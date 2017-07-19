c239b0e59c53e411685f4b46825f20d406de17e5
John Baek <JohnBaek92@Gmail.com>
2017-07-19 10:17:44 -0400

    Modify webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,72 @@
-var path = require("path");
+// var path = require("path");
+//
+// module.exports = {
+//   context: __dirname,
+//   entry: "./frontend/traptitude.jsx",
+//   output: {
+//     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+//     filename: "bundle.js"
+//   },
+//   module: {
+//     loaders: [
+//       {
+//         test: [/\.jsx?$/, /\.js?$/],
+//         exclude: /node_modules/,
+//         loader: 'babel-loader',
+//         query: {
+//           presets: ['es2015', 'react']
+//         }
+//       }
+//     ]
+//   },
+//   devtool: 'source-maps',
+//   resolve: {
+//     extensions: [".js", ".jsx", "*"]
+//   }
+// };
 
+var path = require("path");
+var webpack = require("webpack");
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
+
+// include plugins config
 module.exports = {
   context: __dirname,
   entry: "./frontend/traptitude.jsx",
   output: {
-    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
     filename: "bundle.js"
   },
+  plugins: plugins,
   module: {
     loaders: [
       {
-        test: [/\.jsx?$/, /\.js?$/],
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015', 'react']
+          presets: ['react', 'es2015']
         }
       }
     ]
   },
-  devtool: 'source-maps',
+  devtool: 'source-map',
   resolve: {
     extensions: [".js", ".jsx", "*"]
   }
