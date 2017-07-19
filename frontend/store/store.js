0c9b56ea8ca54e7039454c6a041a07e22a20c2e7
John Baek <JohnBaek92@Gmail.com>
2017-07-19 10:19:34 -0400

    update logger so it's not shown in production

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,13 +1,31 @@
+// import { createStore, applyMiddleware } from 'redux';
+// import thunk from "../middleware/thunk_middleware";
+// import logger from 'redux-logger';
+// import RootReducer from '../reducers/root_reducer';
+//
+// const configureStore = (preloadedState = {}) => (
+//   createStore(
+//     RootReducer,
+//     preloadedState,
+//     applyMiddleware(thunk, logger))
+// );
+//
+// export default configureStore;
+
 import { createStore, applyMiddleware } from 'redux';
 import thunk from "../middleware/thunk_middleware";
-import logger from 'redux-logger';
 import RootReducer from '../reducers/root_reducer';
 
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  // must use 'require' (import only allowed at top of file)
+  const createLogger = require('redux-logger');
+  middlewares.push(createLogger());
+}
+
 const configureStore = (preloadedState = {}) => (
-  createStore(
-    RootReducer,
-    preloadedState,
-    applyMiddleware(thunk, logger))
+  createStore(RootReducer, preloadedState, applyMiddleware(...middlewares))
 );
 
 export default configureStore;
