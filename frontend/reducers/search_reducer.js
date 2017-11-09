0d39d9f4b654380bd90c987f498ee2e641cb78be
John Baek <johnbaek92@gmail.com>
2017-11-09 15:19:42 -0500

    Start search_reducer

---
+++ frontend/reducers/search_reducer.js
@@ -0,0 +1,9 @@
+import { RECEIVE_ALBUM_RESULTS } from '../actions/search_actions';
+
+const _defaultState = {albums: [], tracks: []};
+
+const SearchReducer = (state = _defaultState, action ) => {
+
+};
+
+export default SearchReducer;
