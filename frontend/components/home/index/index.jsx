9b04b3975e958bc4899e044059b8a5731ec4b419
John Baek <johnbaek92@gmail.com>
2018-01-15 16:40:23 -0500

    Remove add album gif

--- frontend/components/home/index/index.jsx
+++ frontend/components/home/index/index.jsx
@@ -63,8 +63,6 @@ class AlbumIndex extends React.Component {
                 <h2 id="featured-info">Featured Info</h2>
                 <div className="gifs-are-here">
                   <img className="anno-demo-gif" src={window.images.anno_demo_gif} />
-                  <img className="add-album-demo" src="https://media.giphy.com/media/l0HU8lPnLw7cUS4Zq/giphy.gif" />
-                </div>
                 </div>
               </div>
               <div className="top-ten-container">
