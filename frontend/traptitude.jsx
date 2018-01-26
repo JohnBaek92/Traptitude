495d1917a77956b1702ca0fd9d5abe6525606012
John Baek <johnbaek92@gmail.com>
2018-01-26 12:24:16 -0500

    remove dispatch and getState from the window

--- frontend/traptitude.jsx
+++ frontend/traptitude.jsx
@@ -12,8 +12,6 @@ document.addEventListener('DOMContentLoaded', () => {
   } else {
     store = configureStore();
   }
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
 });
