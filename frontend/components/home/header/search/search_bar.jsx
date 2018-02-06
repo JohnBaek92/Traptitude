e23ff2091227a279e919fbdf0d2510d839c15c94
John Baek <johnbaek92@gmail.com>
2018-02-06 17:32:50 -0500

    header logo now stays in place for login and logout

--- frontend/components/home/header/search/search_bar.jsx
+++ frontend/components/home/header/search/search_bar.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-const debounce = require('lodash/debounce');
+import React from "react";
+import { Link } from "react-router-dom";
+const debounce = require("lodash/debounce");
 
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-                    searchText: "",
-                    hideSearchResults: true
-                  };
+      searchText: "",
+      hideSearchResults: true
+    };
 
     this.focus = this.focus.bind(this);
     this.handleClick = this.handleClick.bind(this);
@@ -19,7 +19,7 @@ class SearchBar extends React.Component {
 
   componentWillReceiveProps(nextProps) {
     if (this.props.searchText !== nextProps.searchText) {
-      this.setState({searchText: nextProps.searchText}, this.fetchResults)
+      this.setState({ searchText: nextProps.searchText }, this.fetchResults);
     }
   }
 
@@ -32,70 +32,76 @@ class SearchBar extends React.Component {
   }
 
   handleChange(e) {
-    this.setState({searchText: e.currentTarget.value});
-    if(e.currentTarget.value !== "") {
+    this.setState({ searchText: e.currentTarget.value });
+    if (e.currentTarget.value !== "") {
       this.debouncing();
     }
   }
 
   handleClick(e) {
-    this.setState({searchText: ""});
+    this.setState({ searchText: "" });
     $(".search-bar-input").val("");
     this.props.clearAlbums();
   }
 
   focus(boolean) {
-    return(() => {
-      this.setState({hideSearchResults: boolean});
-    });
+    return () => {
+      this.setState({ hideSearchResults: boolean });
+    };
   }
 
   searchAlbumResults() {
-      const results = this.props.albumResults.map( (album, index) => {
-        return(
-          <Link to={'/albums/'+album.id} key={index} onMouseDown={this.handleSubmit}>
-            <div className="album-results"  onClick={this.handleClick}>
-              <img src={album.image_url} className="search-album-image" />
-              <div className="album-results-title-musician">
-                <div className="no-text-transform">{album.title}</div>
-                <div className="no-text-transform">{album.musician}</div>
-              </div>
+    const results = this.props.albumResults.map((album, index) => {
+      return (
+        <Link
+          to={"/albums/" + album.id}
+          key={index}
+          onMouseDown={this.handleSubmit}
+        >
+          <div className="album-results" onClick={this.handleClick}>
+            <img src={album.image_url} className="search-album-image" />
+            <div className="album-results-title-musician">
+              <div className="no-text-transform">{album.title}</div>
+              <div className="no-text-transform">{album.musician}</div>
             </div>
-          </Link>
-      )
+          </div>
+        </Link>
+      );
     });
 
-    if(!this.state.hideSearchResults) {
-      if(this.state.searchText === "") {
+    if (!this.state.hideSearchResults) {
+      if (this.state.searchText === "") {
         return;
       }
-      return(
+      return (
         <div>
           <h4 className="search-results-header">Search Results</h4>
-          <hr className="horizontal-bar"/>
+          <hr className="horizontal-bar" />
           <h5 className="album-results-header">Albums</h5>
-          <ul>
-            {results}
-          </ul>
+          <ul>{results}</ul>
         </div>
-      )
+      );
     } else {
       return;
     }
   }
 
   render() {
-    return(
+    return (
       <div className="search-bar">
         <div className="search-bar">
-          <input onChange={this.handleChange} onBlur={this.focus(true)} onFocus={this.focus(false)} className='search-bar-input' placeholder="Search Albums"></input>
+          <input
+            onChange={this.handleChange}
+            onBlur={this.focus(true)}
+            onFocus={this.focus(false)}
+            className="search-bar-input"
+            placeholder="Search Albums"
+          />
         </div>
-        <div className="search-results">
-          {this.searchAlbumResults()}
-        </div>
+        <div className="search-results">{this.searchAlbumResults()}</div>
       </div>
-    )
+    );
   }
 }
 
-export default SearchBar
+export default SearchBar;
